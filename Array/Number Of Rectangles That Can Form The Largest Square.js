4fab04dd668d09a34952dbe6041b2c12b76b9287
ivan <ivanlisichkin@icloud.com>
2023-08-18 15:15:53 +0400

    feat: solved the problem

---
+++ Array/Number Of Rectangles That Can Form The Largest Square.js
@@ -0,0 +1,22 @@
+var countGoodRectangles = function (rectangles) {
+  let number = 0;
+  let counter = 0;
+  let resArr = rectangles
+    .map((item) => Math.min(...item))
+    .sort((a, b) => a - b)
+    .reverse(); //?
+
+  for (let item of resArr) {
+    if (item >= number) {
+      number = item;
+      counter += 1;
+    }
+  }
+  return counter;
+};
+countGoodRectangles([
+  [5, 8],
+  [3, 9],
+  [5, 12],
+  [16, 5],
+]);
